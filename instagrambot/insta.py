8fe75304f4c0331389c0bd93850158f6b1748ff6
Shailendra Mahadule <120922588+Shailendra-08@users.noreply.github.com>
2023-07-17 12:34:14 +0530

    Instagram bot funtion
    instagram bot function check

---
+++ instagrambot/insta.py
@@ -0,0 +1,20 @@
+
+
+from InstaWebBot import InstaBot
+bot = InstaBot('your_username', 'your_password')
+
+# A bot login is required before any other action will be executed.
+bot.login()
+
+ # The function we can done by using instagram bot
+
+# bot.follow()
+# bot.unfollow()
+# bot.send_dm(message)
+# bot.get_followers()
+# bot.get_picture(number)
+# bot.get_likes()
+# bot.get_liked_by()
+# bot.like_picture()
+# bot.write_comment(message)
+
